0ef67a1ff15dcc01e31baa2660e12ff242383749
Myeonghun Kim <51645938+dooki114@users.noreply.github.com>
2022-02-05 09:56:47 -0500

    face_detection_projects

---
+++ detection_on_cam/face_detection_cam.py
@@ -0,0 +1,33 @@
+#imports
+import cv2
+from random import randrange
+import os
+# Change the current directory 
+# to specified directory
+directory = "/Users/dongsukma/Desktop/ai_tutorial/detection_on_image/"
+os.chdir(directory)
+
+#get a bunch of face data
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+#webcam = cv2.VideoCapture("video file name') #video file
+webcam = cv2.VideoCapture(0) #webcam
+
+#iteration
+while True:
+    successful_frame_read, frame = webcam.read()
+    #convert color to grayscale
+    gray_scaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    face_coordinates = trained_face_data.detectMultiScale(gray_scaled_img)
+    for (x, y , w, h) in face_coordinates:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 256, 0), 2)
+    
+    cv2.imshow("Clever Programmer Face Dectector", frame)
+    key = cv2.waitKey(1) #default: wait on that frame until the key pressed / number: wait for that milliseconds per frame
+
+    if key==81 or key==113: #Q or q key pressed
+        break
+        webcam.release()
+
+#end
+print("code completed")
---
+++ detection_on_image/face_detection_image.py
@@ -0,0 +1,38 @@
+#imports
+import cv2
+from random import randrange
+import os
+
+# Change the current directory 
+# to specified directory
+directory = "/Users/dongsukma/Desktop/ai_tutorial/detection_on_image/"
+os.chdir(directory)
+
+#get a bunch of face data
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+#sample images
+#img = cv2.imread('RDJ.jpeg')
+img = cv2.imread('multiple_faces.jpg')
+
+#convert the target image color to gray
+gray_scaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#detect faces
+face_coordinates = trained_face_data.detectMultiScale(gray_scaled_img)
+#print(face_coordinates)
+
+#draw rectangles
+for (x, y , w, h) in face_coordinates:
+    cv2.rectangle(img, (x,y), (x+w, y+h), (randrange(256), randrange(256), randrange(256)), 2)
+
+#save/show img
+'''
+filename = "face_detected_output.jpg"
+cv2.imwrite(filename, img)
+'''
+cv2.imshow('Face Detector', img)
+cv2.waitKey()
+
+#end
+print("code completed")
